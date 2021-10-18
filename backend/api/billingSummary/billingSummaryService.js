53661a319b08527c932649846989f9d89bef1fca
Lucas Paris <lucashenriqueparis@gmail.com>
2021-10-17 22:22:19 -0300

    Fix query in API

--- backend/api/billingSummary/billingSummaryService.js
+++ backend/api/billingSummary/billingSummaryService.js
@@ -1,4 +1,4 @@
-const ld = require('lodash');
+const _ = require('lodash');
 const billingCycle = require('../billingCycle/billingCycle');
 
 /*
@@ -6,23 +6,26 @@ const billingCycle = require('../billingCycle/billingCycle');
 
     Doc utilização $project:
     https://docs.mongodb.com/manual/reference/operator/aggregation/project/#mongodb-pipeline-pipe.-project
+   
     Doc utilização $group:
     https://docs.mongodb.com/manual/reference/operator/aggregation/group/#mongodb-pipeline-pipe.-group
 */
 
 function getSummary(req, res) {
-    console.log('getSummary');
-    billingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debts: {$sum: "$debts.value"}}
-    },{
-        $group: {_id: null, credit: {$sum: "credit"}, debt: {$sum: "debts"}}
-    },{
-        $project: {_id: 0, credit: 1, debt: 1}
-    }, function(error, result) {
+    billingCycle.aggregate(
+    
+        {
+            $project: {credit: {$sum: "$credits.value"}, debts: {$sum: "$debts.value"}}
+        },{
+            $group: {_id: null, credit: {$sum: "credit"}, debt: {$sum: "debts"}}
+        },{
+            $project: {_id: 0, credit: 1, debt: 1}
+        }
+    , function(error, result) {
         if (error) {
             res.status(500).json({errors: [error]})
         } else {
-            res.json(ld.defaults(result[0], {credit: 0, debt: 0}))
+            res.json(_.defaults(result[0], {credit: 0, debt: 0}))
         }
     })
 }
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://localhost/db_finance');
+module.exports = mongoose.connect('mongodb://127.0.0.1:27017/db_finance',{useMongoClient:true});
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
