7040392a20bbbcd3618c438e305c4c30e6ffe846
Lucas Paris <lucashenriqueparis@gmail.com>
2021-10-31 16:11:49 -0300

    Configuring CORS and consuming API for dashboard viewing

---
+++ angularJS/app/dashboard/dashboardCtrl.js
@@ -0,0 +1,25 @@
+(function() {
+  
+  angular.module('primeiraApp').controller('dashboardCtrl', [
+    '$http',
+    dashboardCtrl
+  ])
+  
+  function dashboardCtrl($http) {
+    
+    const vm = this;
+    vm.getSummary = function () {
+      const url = 'http://localhost:3003/api/billingSummary';
+      $http.get(url).then(function(response) {
+        console.log('response', response);
+        const {credit = 0, debt = 0} = response.data;
+        vm.credit = credit;
+        vm.debt = debt;
+        vm.total = credit - debt;
+      })
+    }
+
+    vm.getSummary();
+
+  }
+})()
--- backend/config/server.js
+++ backend/config/server.js
@@ -3,13 +3,16 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const AllowCors = require('./cors')
 
 // responsável por fazer a interpretação dos que venha a partir da submissão de um formulário 
 server.use(bodyParser.urlencoded({
     extended: true
 }));
-
 server.use(bodyParser.json());
+server.use(AllowCors);
+
+
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`);
